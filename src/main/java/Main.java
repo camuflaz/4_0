439cf932f06532df9c9228f1879ef48f328531cf
camuflaz <73291870+camuflaz@users.noreply.github.com>
2024-05-08 12:45:16 +0000

    4_1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,14 +8,30 @@ Kod bazowy programu Commit4_0:
 */
 
 import java.io.IOException;
-
+import java.util.Scanner;
 class Main {
   public static void main(String[] args) {
     try {
+      Scanner scanner = new Scanner(System.in);
       Service s = new Service();
-      s.addStudent(new Student("Krzysztof", 20));
-      s.addStudent(new Student("Janusz", 40));
-
+      System.out.println("1 - Dodawanie studenta 2 - Wyswietlanie informacji 3 - Usuwanie                 studentow z listy 4 - Wyjscie");
+      int wybor = scanner.nextInt();
+      switch(wybor){
+        case 1:
+          System.out.println("Podaj imie studenta:");
+          String imie = scanner.next();
+          System.out.println("Podaj wiek:");
+          int wiek = scanner.nextInt();
+          s.addStudent(new Student(imie, wiek));
+          break;
+        case 2:
+          
+          break;
+        case 3:
+          break;
+        case 4:
+          break;
+      }
       var students = s.getStudents();
       for(Student current : students) {
         System.out.println(current.ToString());
